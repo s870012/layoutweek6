34ca823d300294c61c582955ce38ddfed4f61b4e
Joey <s8700122000@gamil.ocm>
2024-08-18 03:08:59 +0800

    update

--- main.js
+++ main.js
@@ -3,49 +3,70 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".swiper-article", {
+  navigation: {
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  },
+  breakpoints: {
+    1200:{
+      slidesPerView: 2,
+      spaceBetween: 48,
+    },
+    576:{
+      slidesPerView: 1,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination-custom",
+    clickable: true,
+    type:"fraction",
+  },
+  });
+
+  const swiper1 = new Swiper(".swiper-view", {
     pagination: {
       el: ".swiper-pagination",
-      clickable: true
+      clickable: true,
     },
-    navigation:{
-        prevEl:'.swiper-button-prev',
-        nextEl:'.swiper-button-next',
-    },
-    grid: {
-      rows: 1,
-    },
+    spaceBetween: 24,
     breakpoints: {
-      992: {      
-        grid: { 
-          rows: 1,
-        },
+      768:{
         slidesPerView: 2,
-        spaceBetween: 48,
-      }
+      },
+      576:{
+        slidesPerView: 1,
+      },
     }
   });
 
-  const swiper1 = new Swiper(".swiper", {
+  const swiper2 = new Swiper(".swiper-subscribe", {
     pagination: {
       el: ".swiper-pagination",
       clickable: true
     },
-    navigation:{
-        prevEl:'.swiper-button-prev',
-        nextEl:'.swiper-button-next',
+    grid:{
+      rows:1,
+    },
+    slidesPerView: 1,
+    spaceBetween: 24,
+  });
+
+  const swiper3 = new Swiper(".swiper-blog", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
     },
     grid:{
       rows:1,
     },
     breakpoints: {
-      767: {      
-        slidesPerView: 2,
-        spaceBetween: 48,
-      },
       576:{
-        slidesPerView: 1,
-        spaceBetween: 48,
+        grid:{
+          rows: 3,
+        },
       }
-    }
+    },
+    slidesPerView: 1,
+    spaceBetween: 24,
   });
